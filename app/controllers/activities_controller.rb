b0a218db4a95ae47af6e5893b507a543319b0a99
Savraj Singh <savrajsingh@gmail.com>
2017-03-13 15:49:43 -0400

    Crud on the back end

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,76 +1,47 @@
 class ActivitiesController < ApplicationController
-  before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   # GET /activities
   # GET /activities.json
   def index
-    @activities = Activity.all
+    @location = Location.find(params[:location_id])
+    @activities = @location.activities.all
     render json: @activities
   end
 
-  # GET /activities/1
-  # GET /activities/1.json
-  def show
+  def edit
+    @location = Location.find(params[:location_id])
+    @activity = @location.activities.find(params[:id])
     render json: @activity
   end
 
-  # GET /activities/new
-  def new
-    @activity = Activity.new
+  def show
+    @location = Location.find(params[:location_id])
+    @activity = @location.activities.find(params[:id])
+    render json: @activity
   end
 
-  # GET /activities/1/edit
-  def edit
-  end
-
-  # POST /activities
-  # POST /activities.json
   def create
-    @activity = Activity.new(activity_params)
-
-    respond_to do |format|
-      if @activity.save
-        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
-        format.json { render :show, status: :created, location: @activity }
-      else
-        format.html { render :new }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
-      end
-    end
+    @location = Location.find(params[:location_id])
+    @activity = @location.activities.create(activity_params)
+    render json: @activity
   end
 
-  # PATCH/PUT /activities/1
-  # PATCH/PUT /activities/1.json
   def update
-    respond_to do |format|
-      if @activity.update(activity_params)
-        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
-        format.json { render :show, status: :ok, location: @activity }
-      else
-        format.html { render :edit }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
-      end
-    end
+    @location = Location.find(params[:location_id])
+    @activity = @location.activities.find(params[:id])
+    @activity.update(activity_params)
+    render json: @activity
   end
 
-  # DELETE /activities/1
-  # DELETE /activities/1.json
   def destroy
+    @location = Location.find(params[:location_id])
+    @activity = @location.activities.find(params[:id])
     @activity.destroy
-    respond_to do |format|
-      format.html { redirect_to activities_url, notice: 'Activity was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_activity
-      @activity = Activity.find(params[:id])
+    def activity_params
+      params.require(:activity).permit(:name,:address,:category,:photo_url,:website_url,:description)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def activity_params
-      params.fetch(:activity, {})
-    end
-end
+  end
